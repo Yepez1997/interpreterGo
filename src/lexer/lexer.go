1111d14253afa47c20a0bd03661ff90755fa25db
Aureliano Yepez <ayepez@Aurelianos-MacBook-Pro.local>
2020-01-06 22:34:05 -0800

    lexer helper function to peek at next char

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -4,13 +4,26 @@ package lexer
 // token info at lexer level
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
-	ch           byte
+	position     int  // current position
+	readPosition int  // peek the next position
+	ch           byte // current character examining
 }
 
 // create a lexer input
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
+}
+
+// readChar - helper method
+func (l *Lexer) readChar() {
+	// finished reading @ the end
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	// update the current position to be the position read
+	l.position = l.readPosition
+	l.readPosition += 1
 }
